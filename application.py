6feedf4678cd25e6973a4aa367c9a40df6027e2a
Ragul S V <41230960+Ragul-SV@users.noreply.github.com>
2020-11-19 13:19:10 +0530

    Update application.py

--- application.py
+++ application.py
@@ -39,7 +39,7 @@ app.debug=True
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'skillsdacyberlab@gmail.com'
-app.config['MAIL_PASSWORD'] = '$Cyber123'
+app.config['MAIL_PASSWORD'] = '*****'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 
